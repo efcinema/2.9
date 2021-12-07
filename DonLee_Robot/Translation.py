7d19b4718031a142c4fbe70742e085a697e3675b
efcinema <94225542+efcinema@users.noreply.github.com>
2021-12-07 13:08:30 +0530

    Update Translation.py

--- DonLee_Robot/Translation.py
+++ DonLee_Robot/Translation.py
@@ -42,16 +42,16 @@ class Translation(object):
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@Mo_Tech_YT
+@efcinema
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/Mrk_YT">Muhammed Rk</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/yaduvind">Lucky</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/PR0FESS0R-99/DonLee_Robot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/efcinema/2.9">Click Me</a></i>
 """
